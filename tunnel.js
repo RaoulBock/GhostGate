7db9222d4b65daad9316622b685705c0edf54559
RAOUL BOCK <52930905+RaoulBock@users.noreply.github.com>
2025-10-24 08:26:39 +0200

    change

--- tunnel.js
+++ tunnel.js
@@ -1,23 +1,24 @@
-import localtunnel from "localtunnel";
+// tunnel.js
+import localtunnel from 'localtunnel';
 
-const port = 7108;
-const subdomain = "dcappimperial";
+const port = 7108; // your local app port
+const subdomain = 'dcappimperial'; // optional (try custom name, else random)
 
-async () => {
+(async () => {
   try {
     const tunnel = await localtunnel({
       port,
       subdomain,
     });
 
-    console.log("Tunnel started");
-    console.log(`Public URI ${tunnel.url}`);
-    console.log("Forwarding -> https://dc-bc-app.imperial.local");
+    console.log(🚀 Tunnel started!);
+    console.log(Public URL: ${tunnel.url});
+    console.log(Forwarding -> https://dc-bc-app.imperial.local:${port});
 
-    // tunnel.on("close", () => {
-    //   console.log("Tunnel closed");
-    // });
-  } catch (error) {
-    console.error("ERROR Starting tunnel", error);
-  }
-};
+    tunnel.on('close', () => {
+      console.log('❌ Tunnel closed');
+    });
+  } catch (err) {
+    console.error('Error starting tunnel:', err);
+  }
+})();
