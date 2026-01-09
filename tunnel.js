76fa9ad021b9824601df54d2c589ca2b055ce735
RaoulBock <anthonyraoulbock01@gmail.com>
2026-01-09 10:48:49 +0200

    added variables

--- tunnel.js
+++ tunnel.js
@@ -1,22 +1,24 @@
 import httpProxy from "http-proxy";
 import { spawn } from "child_process";
 
+const PORT = 1234;
+
 const proxy = httpProxy.createProxyServer({
-  target: "https://example.domain.local:7108", // Change to web service link
+  target: `https://example.domain.local:${PORT}`, // Change to web service link and port
   changeOrigin: true,
   secure: false, // ignore self-signed certs
   headers: {
-    host: "dc-bc-app.imperial.local",
+    host: "example.domain.local", // Change to host domain without port
   },
 });
 
-proxy.listen(7108, () => {
+proxy.listen(PORT, () => {
   console.log(
-    "🔁 Proxy running on http://localhost:7108 -> https://example.domain.local:7108" // Change to web service link
+    `🔁 Proxy running on http://localhost:${PORT} -> https://example.domain.local:${PORT}` // Change to web service link and port
   );
 
   // Start Tunnelmole CLI
-  const tmole = spawn("tmole", ["7108"]);
+  const tmole = spawn("tmole", [`${PORT}`]);
 
   tmole.stdout.on("data", (data) => console.log(data.toString()));
   tmole.stderr.on("data", (data) => console.error(data.toString()));
